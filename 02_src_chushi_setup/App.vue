7b60a9e31ee39bcb54766e6a80c095c2f3ab3805
jzq1516 <2864547343@qq.com>
2023-03-10 16:53:46 +0800

    vue3

---
+++ 02_src_chushi_setup/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{name}}</h2>
+  <h2>年龄：{{age}}</h2>
+  <h2>性别：{{sex}}</h2>
+  <button @click="sayHello">说话(Vue3中setup配置的sayHello)</button>
+  <br>
+  <br>
+  <button @click="sayWelcome">说话(Vue2中配置的sayWelcome)</button>
+  <br>
+  <br>
+  <button @click="test1">说话(Vue2中访问Vue3中setup的数据)</button>
+  <br>
+  <br>
+  <button @click="test2">说话(Vue3中访问Vue2中的数据)</button>
+</template>
+
+<script>
+  import { h } from 'vue'
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        sex: '男'
+      }
+    },
+    methods: {
+      sayWelcome() {
+        alert('欢迎来到来学习Vue3！')
+      },
+      test1() {
+        console.log(this.name);
+        console.log(this.age);
+        console.log(this.sex);
+        this.sayHello()
+      }
+    },
+    setup() {
+      let name = '张三'
+      let age = 18
+
+      function sayHello() {
+        alert(`我叫${name}，我${age}岁了，你好啊！`)
+      }
+
+      function test2() {
+        console.log(name);
+        console.log(age);
+        console.log(this.sex);
+        console.log(this.sayWelcome)
+      }
+
+      // 返回一个对象（常用）
+      return {
+        name,
+        age,
+        sayHello,
+        test2
+      }
+
+      // 返回一个函数（渲染函数） 
+      // return () => h('h1', 'App组件')
+
+    }
+  }
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{name}}</h2>
+  <h2>年龄：{{age}}</h2>
+  <h2>工作种类：{{job.type}}</h2>
+  <h2>工作薪资：{{job.salary}}</h2>
+  <button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    setup() {
+      // 数据
+      let name = ref('张三')
+      let age = ref(18)
+      let job = ref({
+        type: '前端工程师',
+        salary: '30k'
+      })
+
+      // 方法
+      function changeInfo() {
+        // name.value = '李四'
+        // age.value = 48
+        job.value.type = 'UI设计师'
+        job.value.salary = '60K'
+        // console.log(name, age);
+      }
+
+      // 返回一个对象（常用）
+      return {
+        name,
+        age,
+        job,
+        changeInfo
+      }
+
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
